035c0b21ce2c5cc608fc98e7edac62ba17170e77
NurbekKurbonov <nurbek.kurbonov.96@gmail.com>
2021-12-24 20:14:33 +0500

    ch(deploy): Deploy basic app

---
+++ startingpage/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+from django.contrib import messages
+
+def index(request):
+    return render(request, 'startingpage/index.html')
