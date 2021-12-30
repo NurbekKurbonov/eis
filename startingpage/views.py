a618bf8985e6992beeb0638feb5b32cd8428944a
NurbekKurbonov <nurbek.kurbonov.96@gmail.com>
2021-12-30 07:42:08 +0500

    Yangilanish

--- startingpage/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.contrib import messages
-from django.shortcuts import render, redirect
-
-from django.contrib import messages
-
-def index(request):
-    return render(request, 'startingpage/index.html')
